bd44f8b1aaf8f043f9a66af5de4cad21b4e55203
Lucas Andrés Davila <lucaslan490@gmail.com>
2025-06-08 12:03:26 -0300

    Añadiendo paleta de colores generada

--- py-projects/image-color-extraction/main.py
+++ py-projects/image-color-extraction/main.py
@@ -1,16 +1,22 @@
 from PIL import Image
-Image.open('.jpg')
-
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
-image = mpimg.imread('.jpg')
-w, h, d = tuple(image.shape)
-pixels = np.reshape(image, (w * h, d))
 from sklearn.cluster import KMeans
-n_colors = 10 # listo para cambiar cantidad de colores con Kmeans
+
+image = mpimg.imread('example.jpg')
+
+if image.dtype == np.float32 or image.max() <= 1.0:
+    image = (image * 255).astype(np.uint8)
+
+w, h, d = image.shape
+pixels = image.reshape((w * h, d))
+
+n_colors = 10
 model = KMeans(n_clusters=n_colors, random_state=42).fit(pixels)
 palette = np.uint8(model.cluster_centers_)
+
 plt.imshow([palette])
+plt.axis('off')
+plt.title('Paleta de colores obtenidos')
 plt.show()
